c95b7c2c32629b9ebb6ec7905683cf8ca2cb2afc
andreas mullen <amdymul@gmail.com>
2026-06-15 23:17:54 +0100

    Moving onto parsing

--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -35,16 +35,37 @@ public class GenerateAst
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
         // The AST classes
         for (String type : types)
         {
             String className = type.split(":")[0].trim();
-            String fields =  type.split(":")[1].trim();
+            String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
 
+        // The base accept() method.
+        // The accept acts as a route finder mapping the correct visitor to the type where
+        // the visitor is the function being added to the set of types.
+        writer.println();
+        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+
         writer.println("}");
         writer.close();
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types)
+    {
+        writer.println("  interface Visitor<R> {");
+
+        for (String type : types)
+        {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("  }");
     }
 
     private static void defineType(PrintWriter writer, String baseName, String className, String fieldList)
@@ -62,6 +83,14 @@ public class GenerateAst
             writer.println("      this." + name + " = " + name + ";");
         }
 
+        writer.println("    }");
+
+        // Visitor pattern.
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("      return visitor.visit" +
+                className + baseName + "(this);");
         writer.println("    }");
 
         // Fields.
